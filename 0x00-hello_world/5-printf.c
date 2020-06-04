4e9c03d84ecc1262b58534c805aaa197715ca7a2
JwowSquared <JwowRS@hotmail.com>
2020-06-04 03:55:02 +0000

    added task 5 source code

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints sentence using printf
+ *
+ * Return: exit function
+ */
+int main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art,\n");
+return (0);
+}
