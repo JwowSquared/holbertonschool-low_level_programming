4880f5530826638aea32618f2976545254b38513
JwowSquared <JwowRS@hotmail.com>
2020-06-04 16:47:43 +0000

    added task 3 source code

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _islower - checks if c is lowercase
+ *
+ * @c: the number to be checked
+ *
+ * Return: 1 if c is lowercase
+ */
+int _islower(int c)
+{
+	if (c < 97)
+		return (1);
+
+	return (0);
+}
