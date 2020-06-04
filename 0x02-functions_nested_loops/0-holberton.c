7d15644cdf9a127f6627fa3e05fcdab00e824323
JwowSquared <JwowRS@hotmail.com>
2020-06-04 14:56:10 +0000

    fixed code to use exclusively _putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,7 +7,17 @@
  */
 int main(void)
 {
-	_putchar("Testing\n");
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
 	return (0);
 }
 
