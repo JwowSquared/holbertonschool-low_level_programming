2f54c12001095fbdcebbad7ada9ef47389c366ca
JwowSquared <JwowRS@hotmail.com>
2020-06-04 20:43:51 +0000

    abandoned single line conditional statement

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,7 +12,24 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 && !(year % 100 == 0)) || year % 400 == 0)
+	int common;
+
+	if (year % 4 == 0)
+	{
+		if (year % 100 == 0)
+		{
+			if (year % 400 == 0)
+				common = 0;
+			else
+				common = 1;
+		}
+		else
+			common = 0;
+	}
+	else
+		common = 1;
+/*	if ((year % 4 == 0 && !(year % 100 == 0)) || year % 400 == 0) */
+	if (common == 0)
 	{
 		if (month >= 2 && day >= 60)
 		{
