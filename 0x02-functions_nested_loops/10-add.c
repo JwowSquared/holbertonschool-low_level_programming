161ad918bc4e165efa2fcb27326f84d6cc79006d
JwowSquared <JwowRS@hotmail.com>
2020-06-04 18:02:22 +0000

    added task 10 source code

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * add - adds two numbers together and returns the result
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: result of a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
