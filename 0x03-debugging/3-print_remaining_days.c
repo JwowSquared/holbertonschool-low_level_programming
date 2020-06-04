ad77f9f23d7d3433f6a10b305e466e1ed33cf5ca
JwowSquared <JwowRS@hotmail.com>
2020-06-04 21:01:30 +0000

    maybe fixed bug

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -13,17 +13,17 @@
 void print_remaining_days(int month, int day, int year)
 {
 	int common;
-
-	if (year % 4 != 0)
-		common = 1;
-	else if (year % 100 != 0)
-		common = 0;
-	else if (year % 400 != 0)
-		common = 1;
-	else
-		common = 0;
-/*	if ((year % 4 == 0 && !(year % 100 == 0)) || year % 400 == 0) */
-	if (common == 0)
+/*
+*	if (year % 4 != 0)
+*		common = 1;
+*	else if (year % 100 != 0)
+*		common = 0;
+*	else if (year % 400 != 0)
+*		common = 1;
+*	else
+*		common = 0;
+*/
+	if ((year % 4 == 0 && !(year % 100 == 0)) || year % 400 == 0)
 	{
 		if (month >= 2 && day >= 60)
 		{
