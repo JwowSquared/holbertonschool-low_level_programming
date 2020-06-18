3958e16b43bb6ae5cfcd8698a3c2dddcc3055fce
JwowSquared <JwowRS@hotmail.com>
2020-06-18 04:30:06 +0000

    added task 9 source code

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* main - interview question Fizz Buzz
+*
+* Return: exit function
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		if (i % 5 == 0)
+			printf("Buzz");
+		if (i % 3 != 0 && i % 5 != 0)
+			printf("%d", i);
+		if (i != 100)
+			putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
