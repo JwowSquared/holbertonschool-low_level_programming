2369c9ac3da43461049c64fbdbf78d9e361ccb44
JwowSquared <JwowRS@hotmail.com>
2020-06-18 02:45:44 +0000

    added task 4 source code

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+* print_most_numbers - prints the numbers 0 - 9 but not 2 or 4
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 2 || i == 4)
+			continue;
+
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
