ed3fb02e9e71863518159a6c7b721383d7a255d6
JwowSquared <JwowRS@hotmail.com>
2020-06-18 02:21:09 +0000

    added task 0 source code

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+* _isupper - checks for uppercase character
+* @c: character to be checked
+*
+* Return: 1 if c is uppercase, 0 otherwise
+*/
+int _isupper(int c)
+{
+	if (c >= 97)
+		return 1;
+	else
+		return 0;
+}
