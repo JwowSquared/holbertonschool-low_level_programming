d75aa67c18c907f55e374a1b3f5a4ef34f457e93
JwowSquared <JwowRS@hotmail.com>
2020-06-18 02:32:06 +0000

    added task 2 source code

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+* mul - multiplies two input integers
+* @a: first input
+* @b: second input
+*
+* Return: result of a * b
+*/
+int mul(int a, int b)
+{
+	return a * b;
+}
