dc2a161b38e485e42145a948bbb934236c6f3a75
JwowSquared <JwowRS@hotmail.com>
2020-06-20 08:41:04 +0000

    added advanced task 9's source code

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,56 @@
+#include "holberton.h"
+
+/**
+* _exp - allows me to perform exponential operations
+* @base: base
+* @ex: exponent
+*
+* Return: base^ex
+*/
+int _exp(int base, int ex)
+{
+	int result = 1;
+
+	while (ex-- > 0)
+		result = result * base;
+
+	return (result);
+}
+
+/**
+* print_number - prints the input integer using only _putchar
+* @n: integer to be printed
+*/
+void print_number(int n)
+{
+	int input;
+	int length = 0;
+	int result;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+
+	input = n;
+
+	while (input > 0)
+	{
+		input = input / 10;
+		length++;
+	}
+
+	while (length > 0)
+	{
+		result = (n / _exp(10, length - 1)) % 10;
+		_putchar(result + 48);
+		length--;
+	}
+}
