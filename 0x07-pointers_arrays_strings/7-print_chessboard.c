a65bdd10575ffa405e0d4d99611ee4d93afb144d
JwowSquared <JwowRS@hotmail.com>
2020-06-28 12:59:10 +0000

    added task 6 source code

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+* print_chessboard - prints nested array
+* @a: nested array to be printed
+*/
+void print_chessboard(char (*a)[8])
+{
+	int i = 0;
+	int j;
+
+	while (i < 8)
+	{
+		j = 0;
+		while (j < 8)
+		{
+			_putchar(a[i][j]);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
