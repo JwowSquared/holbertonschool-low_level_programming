467c225c8025047028ab433b59715fad5307922b
JwowSquared <JwowRS@hotmail.com>
2020-06-28 12:15:59 +0000

    added task 2 source code

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+* _strchr - locates a character in a string
+* @s: pointer to string to search
+* @c: character to locate
+*
+* Return: pointer to first occurance of c, else NULL
+*/
+char *_strchr(char *s, char c)
+{
+	char *find = s;
+
+	while (*find)
+	{
+		if (*find == c)
+			return (find);
+
+		find++;
+	}
+
+	return (0);
+}
