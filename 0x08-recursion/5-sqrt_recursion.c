ba9c7a8400263ecf790614290c7094091d20e77a
JwowSquared <JwowRS@hotmail.com>
2020-06-28 23:27:29 +0000

    added task 5 source code

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+int check(int n, int x)
+{
+	if ((float)n / (float)x == (float)x)
+		return (x);
+	if ((float)n / (float)x < (float)x)
+		return (-1);
+
+	return (check(n, x + 1));
+}
+
+/**
+* _sqrt_recursion - returns the square root of n using recurison
+* @n: number to be.. square rooted?
+*
+* Return: square root of n, else -1 when n doesnt have one
+*/
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+
+	return (check(n, 1));
+}
