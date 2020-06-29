73cd4951b465e800e0e83e04e1b336920f53e5ff
JwowSquared <JwowRS@hotmail.com>
2020-06-29 22:26:56 +0000

    added task 1 source code

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - prints the number of command arguments
+* @argc: number of input arguments
+* @argv: array of input arguments
+*
+* Return: exit function
+*/
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
