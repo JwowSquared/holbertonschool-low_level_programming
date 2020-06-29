7fb965bfe3bea1b153c2e054ce7843264f0a9f2b
JwowSquared <JwowRS@hotmail.com>
2020-06-29 22:53:40 +0000

    added task 4 source code

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - adds positive numbers and prints them to the screen
+* @argc: number of command arguments
+* @argv: array of command arguments
+*
+* Return: 0 on success, 1 on Error
+*/
+int main(int argc, char *argv[])
+{
+	int i = 1;
+	int result = 0;
+	int num;
+
+	if (argc <= 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	while (i < argc)
+	{
+		num = atoi(argv[i]);
+		if (num == 0 && *argv[i] != '0')
+		{
+			printf("Error\n");
+			return (1);
+		}
+		result += num;
+		i++;
+	}
+
+	printf("%d\n", result);
+
+	return (0);
+}
