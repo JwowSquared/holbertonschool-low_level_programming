3499e3dff7a5955a862b767e0e5cdca4eba9220a
JwowSquared <JwowRS@hotmail.com>
2020-06-29 15:27:48 +0000

    fixed betty implementation

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,11 @@
 #include "holberton.h"
 
+/**
+* _atoi - parses input string s and returns first integer found
+* @s: pointer to string to be parsed
+*
+* Return: first integer found in s, else 0
+*/
 int _atoi (char *s)
 {
 	int result = 0;
