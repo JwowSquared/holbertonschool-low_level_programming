71481abcee12b2660db87ca04f2c14823bac6656
JwowSquared <JwowRS@hotmail.com>
2020-06-29 22:21:35 +0000

    added task 0 source code

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - prints the name of this file to stdout
+*
+* Return - exit function
+*/
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
