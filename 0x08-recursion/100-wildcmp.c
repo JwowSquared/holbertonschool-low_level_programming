a23f4eac61ba1875ab53e1683146b425f37482a4
JwowSquared <JwowRS@hotmail.com>
2020-06-30 15:43:24 +0000

    removed function that was not needed

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 
 /**
-* helper - recursively compares two strings, one with wildcard expansion
+* wildcmp - recursively compares two strings, one with wildcard expansion
 * @s1: base string to compare against
 * @s2: string potentially containing wildcards
 *
 * Return: 1 if match, else 0
 */
-int helper(char *s1, char *s2)
+int wildcmp(char *s1, char *s2)
 {
 	if (!*s1 && !*s2)
 		return (1);
@@ -19,22 +19,10 @@ int helper(char *s1, char *s2)
 		return (0);
 
 	if (*s2 == '*')
-		return (helper(s1 + 1, s2) || helper(s1, s2 + 1));
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 
 	if (*s1 == *s2)
-		return (helper(s1 + 1, s2 + 1));
+		return (wildcmp(s1 + 1, s2 + 1));
 	else
 		return (0);
-}
-
-/**
-* wildcmp - compares two strings to determine if they match
-* @s1: base string to compare against
-* @s2: string potentially containing wildcards
-*
-* Return: 1 if match, else 0
-*/
-int wildcmp(char *s1, char *s2)
-{
-	return (helper(s1, s2));
 }
