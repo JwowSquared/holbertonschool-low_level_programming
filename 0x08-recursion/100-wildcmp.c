9731b4c4b22a1ccd7b067983abc71b4cf1402063
JwowSquared <JwowRS@hotmail.com>
2020-06-30 15:50:26 +0000

    added notes for understanding

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -9,18 +9,24 @@
 */
 int wildcmp(char *s1, char *s2)
 {
+	/* if we've reached the end of both strings, success!*/
 	if (!*s1 && !*s2)
 		return (1);
 
+	/*if end of s1, s2 is currently wildcard, and s2 + 1 is the end, success!*/
 	if (*s1 == '\0' && *s2 == '*' && *(s2 + 1) == '\0')
 		return (1);
 
+	/*if its the end of either s1 or s2 at this point, failure*/
 	if (!*s1 || !*s2)
 		return (0);
 
+	/*if s2 is currently wildcard, try to advance s1, then try to advance s2*/
+	/*this logic accounts for wildcards that = 0, or wildcards that = 1+ chars*/
 	if (*s2 == '*')
 		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 
+	/*checks match, if no match then failure, else continue forward on both!*/
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 	else
