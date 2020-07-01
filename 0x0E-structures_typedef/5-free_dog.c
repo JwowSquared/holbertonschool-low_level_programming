4d887b9cbab34d6f7109f8cb0b325770db3828b2
JwowSquared <JwowRS@hotmail.com>
2020-07-01 22:51:59 +0000

    added task 5 source code

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+* free_dog - properly frees all memory associated with a dog_t object
+* @d: pointer to dog_t object to free
+*/
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
