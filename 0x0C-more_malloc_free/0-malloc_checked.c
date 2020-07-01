338d55e73371b3a7415ac91a81f885b9d803cf64
JwowSquared <JwowRS@hotmail.com>
2020-07-01 15:41:36 +0000

    added task 0 source code

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+* malloc_checked - calls malloc with input b
+* @b: amount of bytes to allocate
+*
+* Return: result of malloc
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *out = malloc(b);
+	if (out == NULL)
+	{
+		free(out);
+		exit(98);
+	}
+
+	return (out);
+}
