687e798f0063d86478c4a690e4fe3b6205a9a794
JwowSquared <JwowRS@hotmail.com>
2020-07-01 22:51:12 +0000

    added header file with include guards, function prototypes, and anything else for tasks 0 through 5

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _DOG_
+#define _DOG_
+
+typedef struct dog {
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /* _DOG_ */
