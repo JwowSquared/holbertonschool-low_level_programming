c3b900c6937656e38a575615dd379301cd7d9588
JwowSquared <JwowRS@hotmail.com>
2020-07-02 19:28:41 +0000

    added description for betty

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,8 @@
 #include "function_pointers.h"
 /**
+* print_name - prints name string with input functionality
+* @name: name to output
+* @f: function pointer to print name
 */
 void print_name(char *name, void (*f)(char *))
 {
