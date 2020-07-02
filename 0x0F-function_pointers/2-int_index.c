e4bc7fff164dc2c330ef999dd4c82df913d745f5
JwowSquared <JwowRS@hotmail.com>
2020-07-02 17:22:45 +0000

    added task 2 source code

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+* int_index - returns the index of the first element where cmp doesnt return 0
+* @array: array to look through
+* @size: length of array
+* @cmp: function pointer that evaluates each element of the array
+*
+* Return: index of first match, else -1 if no match or size is 0 or less
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, result;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		result = cmp(array[i]);
+		if (result != 0)
+			return (i);
+	}
+
+	return (-1);
+}
