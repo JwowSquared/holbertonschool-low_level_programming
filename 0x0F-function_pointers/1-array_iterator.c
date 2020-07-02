ea961b1cfe696bfed8480db75050c468cee5f94c
JwowSquared <JwowRS@hotmail.com>
2020-07-02 19:37:40 +0000

    added function description and correctly account for unhandleable inputs

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,8 +1,17 @@
 #include "function_pointers.h"
 
+/**
+* array_iterator - applies functionality to each member of an array
+* @array: array to modify
+* @size: length of array
+* @action: function pointer to apply to array
+*/
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
+
+	if (array == NULL || size <= 0 || action == NULL)
+		return;
 
 	for (i = 0; i < size; i++)
 		action(array[i]);
