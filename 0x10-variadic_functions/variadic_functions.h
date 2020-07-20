b8fc98afb54f45a4c0b780b94ec319533e1c600c
JwowSquared <JwowRS@hotmail.com>
2020-07-20 04:55:30 +0000

    updated for new print_all

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,9 +3,19 @@
 
 #include <stdarg.h>
 
+typedef struct specifier
+{
+	char *key;
+	void (*f)(va_list);
+} spec;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_c(va_list input);
+void print_i(va_list input);
+void print_f(va_list input);
+void print_s(va_list input);
 
 #endif /* _V_FUNCTIONS_ */
