11951db7e3e2c4f837553279505ff864d54edd68
JwowSquared <JwowRS@hotmail.com>
2020-07-22 17:28:54 +0000

    added task 3 source code

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+* add_node_end - creates a new node and slaps it on the end of a list
+* @head: pointer to current head of the list
+* @str: string to set in the new node
+*
+* Return: pointer to new node
+*/
+list_t *add_node_end(list_t **head, const char *str)
+{
+	int len = 0;
+	list_t *node;
+	list_t *new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	if(*head == NULL)
+		*head = new;
+
+	node = *head;
+	while (node->next)
+		node = node->next;
+
+	node->next = new;
+
+	new->next = NULL;
+	new->str = strdup(str);
+	if (new->str != NULL)
+		while (new->str[len])
+			len++;
+	new->len = len;
+	return (new);
+}
