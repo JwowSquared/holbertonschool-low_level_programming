81e5a870f02adaf8eb87d348108bb65527c8fe81
JwowSquared <JwowRS@hotmail.com>
2020-07-22 16:52:54 +0000

    fixed case where input string is NULL

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,12 +12,19 @@ list_t *add_node(list_t **head, const char *str)
 	int len = 0;
 	list_t *new = malloc(sizeof(list_t));
 
+
 	if (new == NULL)
 		return (NULL);
 
-	new->str = strdup(str);
-	while (new->str[len])
-		len++;
+	if (str == NULL)
+		new->str = NULL;
+	else
+	{
+		new->str = strdup(str);
+		while (new->str[len])
+			len++;
+	}
+
 	new->len = len;
 	new->next = *head;
 	(*head) = new;
