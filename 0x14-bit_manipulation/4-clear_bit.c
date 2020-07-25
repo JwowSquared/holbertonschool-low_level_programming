4bfb8853f9d98a28b64d0a39b8aeba208abe42ff
JwowSquared <Jdwagner1@hotmail.com>
2020-07-25 12:46:54 +0000

    added task 4 source code

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+* clear_bit - sets bit at index to 0
+* @n: pointer to number to modify
+* @index: index of number to modify
+*
+* Return: 1 on success, else -1
+*/
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int i;
+
+	if (index > 63)
+		return (-1);
+
+	i = 1 << index;
+	if ((i & (*n)) > 0)
+		(*n) -= i;
+	
+	return (1);
+}
