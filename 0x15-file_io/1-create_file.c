97c35b19283b7957b23aa6c18e0a3ae8adbaf2e9
JwowSquared <Jdwagner1@hotmail.com>
2020-08-03 23:01:40 +0000

    fixed error return and does not attempt write if text_content is NULL

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,24 +1,33 @@
 #include "holberton.h"
 
 /**
+* create_file - creates or overwrites filename with text_content
+* @filename: file to create
+* @text_content: text to fill filename with
+*
+* Return: 1 on success, else -1
 */
 int create_file(const char *filename, char *text_content)
 {
 	int flag, num_w, tc_len = 0;
 
 	if (filename == NULL)
-		exit(0);
+		exit(-1);
+
+
+	flag = open(filename, O_CREAT | O_WRONLY, 0600);
+	if (flag == -1)
+		exit(-1);
+
+	if (text_content == NULL)
+		exit(1);
 
 	while (text_content[tc_len])
 		tc_len++;
 
-	flag = open(filename, O_CREAT | O_WRONLY, 0600);
-	if (flag == -1)
-		exit(0);
-
 	num_w = write(flag, text_content, tc_len);
 	if (num_w == -1)
-		exit(0);
+		exit(-1);
 
 	return (1);
 }
