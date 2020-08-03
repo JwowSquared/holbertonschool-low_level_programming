e1cb3526bebece46a22caf3250d6cc9a91cf58ad
JwowSquared <Jdwagner1@hotmail.com>
2020-08-03 22:16:56 +0000

    updated betty

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,9 +1,17 @@
 #include "holberton.h"
 
+/**
+* read_textfile - reads filename and prints to std_out
+* @filename: name of file
+* @letters: max amount of letters to print
+*
+* Return: amount of bytes written by write, else -1
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t flag, total;
 	char *buffer = malloc(sizeof(char) * (letters + 1));
+
 	if (buffer == NULL)
 		exit(0);
 
