342110c73e4e7fca028d03e7ed0c1cf2f5c9b856
JwowSquared <Jdwagner1@hotmail.com>
2020-08-03 22:34:27 +0000

    not sure what this would fix honestly

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -24,7 +24,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		exit(0);
 
 	num_r = read(flag, buffer, letters);
-	if (num_r == -1)
+	if (num_r == -1 || num_r > letters)
 	{
 		free(buffer);
 		exit(0);
