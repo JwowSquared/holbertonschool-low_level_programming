b05b68646551affa8f8cd4e799c386ae8fbcc7d1
JwowSquared <Jdwagner1@hotmail.com>
2020-10-01 00:23:33 +0000

    removed my now un needed function

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -23,23 +23,3 @@ hash_table_t *hash_table_create(unsigned long int size)
 	out->size = size;
 	return (out);
 }
-
-/**
-* new_hash_node - creates a new blank hash node
-* @prev: ironically, what the new node->next will be
-*
-* Return: pointer to new node, else NULL
-*/
-hash_node_t *new_hash_node(hash_node_t *prev)
-{
-	hash_node_t *out;
-
-	out = malloc(sizeof(hash_node_t));
-	if (out == NULL)
-		return (NULL);
-
-	out->key = NULL;
-	out->value = NULL;
-	out->next = prev;
-	return (out);
-}
