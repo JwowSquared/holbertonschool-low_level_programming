87e5f0d85c51873a122c0f4b3e9dbc52fed4c8ed
JwowSquared <Jdwagner1@hotmail.com>
2020-10-01 05:51:51 +0000

    added task 4 source code

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_get - gets value associated with key
+* @ht: hash table to pull from
+* @key: key to match with
+*
+* Return: value paired with key, else NULL
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *list;
+	unsigned long int idx;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	idx = key_index((unsigned char *)key, ht->size);
+	list = ht->array[idx];
+
+	while (list != NULL)
+	{
+		if (!strcmp(key, list->key))
+			return (list->value);
+		list = list->next;
+	}
+
+	return (NULL);
+}
